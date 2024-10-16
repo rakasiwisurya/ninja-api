b82573f19033e7a9b4cec632ed622b74b8e8859e
Rakasiwi Surya <rakasiwi.surya@gmail.com>
2024-10-16 13:57:59 +0700

    done controllers

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
 
 @Controller('ninjas')
-export class NinjasController {}
+export class NinjasController {
+    // GET /ninjas?type=fast --> []
+    @Get()
+    getNinjas(@Query('type') type: string) {
+        return [
+            {
+                type
+            }
+        ];
+    }
+
+    // GET /ninjas/:id {...}
+    @Get(':id')
+    getOneNinja(@Param('id') id: string) {
+        return {
+            id
+        };
+    }
+
+    // POST /ninjas
+    @Post()
+    createNinja(@Body() createNinjaDto: CreateNinjaDto) {
+        return {
+            name: createNinjaDto.name
+        };
+    }
+
+    // PUT /ninjas/:id --> {...}
+    @Put(':id')
+    updateNinja(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto) {
+        return {
+            id,
+            name: updateNinjaDto.name
+        };
+    }
+
+    // DELETE /ninjas/:id
+    @Delete(':id')
+    removeNinja(@Param('id') id: string) {
+        return {
+            id
+        };
+    }
+}
